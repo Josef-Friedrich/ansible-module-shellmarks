d17f11bef02689054509e4758f55abcb50797e44
Josef Friedrich <josef@friedrich.rocks>
2019-03-12 16:39:51 +0100

    Test ansible-doc

---
+++ test/test_function_subprocess.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+from ansible.compat.tests import unittest
+
+
+old_env = os.getenv('ANSIBLE_LIBRARY')
+
+if old_env:
+    os.putenv('ANSIBLE_LIBRARY', old_env + ':' + os.getcwd())
+else:
+    os.putenv('ANSIBLE_LIBRARY', os.getcwd())
+
+
+class TestFunctionalWithSubprocess(unittest.TestCase):
+
+    def test_ansible_doc(self):
+        output = subprocess.check_output(['ansible-doc', 'shellmarks'])
+        output = output.decode('utf-8')
+        self.assertIn(
+            'commonly used directories',
+            output
+        )
