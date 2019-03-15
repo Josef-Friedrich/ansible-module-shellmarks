fb251eebceb1047ef490cd1dd50cb4f5e2e86545
Josef Friedrich <josef@friedrich.rocks>
2019-03-15 21:37:38 +0100

    Add more tests

--- test/test_integration_replace_home.py
+++ test/test_integration_replace_home.py
@@ -53,3 +53,76 @@ class TestReplaceHome(unittest.TestCase):
                 },
             ]
         )
+
+    def test_replace_home_true_check_mode_true(self):
+        sdirs = self.create_sdirs_file()
+        mock_objects = mock_main(params={'replace_home': True, 'sdirs': sdirs},
+                                 check_mode=True)
+
+        lines = read(sdirs)
+        self.assertNotIn('$HOME', lines[0])
+        mock_objects['module'].exit_json.assert_called_with(
+            changed=True
+        )
+
+    def test_replace_home_true_state_present_check_mode_true(self):
+        sdirs = self.create_sdirs_file()
+        mock_objects = mock_main(
+            params={
+                'replace_home': True,
+                'sdirs': sdirs,
+                'path': DIR1,
+                'mark': 'dir1',
+            },
+            check_mode=True
+        )
+
+        lines = read(sdirs)
+        self.assertNotIn('$HOME', lines[0])
+        mock_objects['module'].exit_json.assert_called_with(
+            changed=True,
+            changes=[
+                {
+                    'action': 'add',
+                    'mark': 'dir1',
+                    'path': DIR1,
+                },
+            ]
+        )
+
+    def test_replace_home_false_check_mode_false(self):
+        sdirs = self.create_sdirs_file()
+        mock_objects = mock_main(params={'replace_home': False, 'sdirs':
+                                         sdirs},
+                                 check_mode=False)
+
+        lines = read(sdirs)
+        self.assertNotIn('$HOME', lines[0])
+        mock_objects['module'].exit_json.assert_called_with(
+            changed=False
+        )
+
+    def test_replace_home_false_state_present_check_mode_false(self):
+        sdirs = self.create_sdirs_file()
+        mock_objects = mock_main(
+            params={
+                'replace_home': False,
+                'sdirs': sdirs,
+                'path': DIR1,
+                'mark': 'dir1',
+            },
+            check_mode=False
+        )
+
+        lines = read(sdirs)
+        self.assertNotIn('$HOME', lines[0])
+        mock_objects['module'].exit_json.assert_called_with(
+            changed=True,
+            changes=[
+                {
+                    'action': 'add',
+                    'mark': 'dir1',
+                    'path': DIR1,
+                },
+            ]
+        )
